8c6632cb42f29ff4662ceaab955cb1d3d57d8054
Martin <mjtitus06@gmail.com>
2025-05-19 14:06:47 +0100

    Completed the catalogue.js file. Added a parent class called School, 3 subclasses and a seperate class to hold all instances of schools.

--- subjects/JavaScript/lessons/Classes/catalogue.js
+++ subjects/JavaScript/lessons/Classes/catalogue.js
@@ -21,11 +21,13 @@ class School{
     }
     //methods:
     quickFacts(){
-
+        console.log(`${this.name} educates ${this.numberOfStudents} students at the ${this.level} school level.`)
     }
     //static method that can only be called on the School class:
-    pickSubstituteTeacher(){
+    static pickSubstituteTeacher(substituteTeachers){
+        let randomNum = Math.floor(Math.random() * substituteTeachers.length)
 
+        return substituteTeachers[randomNum];
     }
 };
 
@@ -66,6 +68,51 @@ class High extends School{
 };
 
 //Instances:
-const firstHighSchool = new High('FairBurn HighSchool', 'high', 1650,);
-firstHighSchool.addSportsTeam('Lions')
-console.log(firstHighSchool);
+//Highschool
+const firstHighSchool = new High('FairBurn High School', 'high', 1650);
+
+firstHighSchool.addSportsTeam('Lions') //adding a sports team this school instance.
+console.log(firstHighSchool); //logging the school
+firstHighSchool.quickFacts(); //calling the quickFacts method on the school
+
+//Primaryschool
+const lorraineHansbury = new Primary('Lorraine Hansbury', 'primary', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.')
+
+lorraineHansbury.quickFacts(); //calling the quickFacts method on the school
+const sub = School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+console.log('Your substitute teacher is: ' + sub);
+
+//2nd HighSchool instance:
+const alSmith = new High('Al E. Smith', 'high', 415);
+
+alSmith.addSportsTeam(['Baseball', 'Basketball', 'Volleyball', 'Track and Field']); //adding an array of sport teams
+console.log(alSmith); //logging the school
+console.log('Your sports teams are: ' + alSmith.sportsTeam); //logging the sports teams only from the schoo
+
+
+//Extra practice:
+//Making a class to hold a collection of schools:
+class SchoolCatalog{
+    constructor(){
+        this._schoolCatalog = [];
+    }
+    //getters:
+    get schoolCatalog(){
+        return this._schoolCatalog;
+    }
+    //method:
+    addToSchoolCatalog(school){
+        this._schoolCatalog.push(school);
+    }
+};
+
+//Instance of school catalog:
+const catalogOfPrimarySchools = new SchoolCatalog();
+
+//Adding multiple school instances to the catalog class:
+catalogOfPrimarySchools.addToSchoolCatalog(alSmith);
+catalogOfPrimarySchools.addToSchoolCatalog(lorraineHansbury);
+catalogOfPrimarySchools.addToSchoolCatalog(firstHighSchool);
+
+//logging the catalog of schools:
+console.log(catalogOfPrimarySchools);
