aab1d5a1aafe88699289c65b88d58bf60993f1e3
Martin <mjtitus06@gmail.com>
2025-06-10 15:57:30 +0100

    Added working links for the posters in the array of objects in the movie_rating project

---
+++ subjects/React/practice_projects--props/movie-ratings/src/assets/movieArray.js
@@ -0,0 +1,62 @@
+const movies = [
+  {
+    title: "The Shawshank Redemption",
+    poster: "https://images-eu.ssl-images-amazon.com/images/I/51EZXDOXBfL._AC_UL210_SR210,210_.jpg",
+    rating: "9.3/10",
+    review: "A moving story of hope and friendship set in a prison."
+  },
+  {
+    title: "The Godfather",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGx14ZL01dtg1D6KtW08pLdG-iOkZ0lnx6VQ&s",
+    rating: "9.2/10",
+    review: "A classic mafia drama with powerful performances."
+  },
+  {
+    title: "The Dark Knight",
+    poster: "https://static.wikia.nocookie.net/live-action/images/f/f1/The_Dark_Knight_Rises_2012_DVD_Cover.PNG/revision/latest?cb=20161208120559",
+    rating: "9.0/10",
+    review: "A thrilling superhero film with a legendary villain."
+  },
+  {
+    title: "Pulp Fiction",
+    poster: "https://e7.pngegg.com/pngimages/899/728/png-clipart-uma-thurman-pulp-fiction-film-poster-film-poster-pulp-poster-film-poster.png",
+    rating: "8.9/10",
+    review: "A stylish, nonlinear crime story with unforgettable dialogue."
+  },
+  {
+    title: "Forrest Gump",
+    poster: "https://image.tmdb.org/t/p/original/saHP97rTPS5eLmrLQEcANmKrsFl.jpg",
+    rating: "8.8/10",
+    review: "A heartwarming tale of an extraordinary man's journey."
+  },
+    {
+    title: "Inception",
+    poster: "https://e7.pngegg.com/pngimages/1003/126/png-clipart-film-poster-art-actor-actor-celebrities-poster-thumbnail.png",
+    rating: "8.8/10",
+    review: "A mind-bending thriller that blurs the line between dream and reality."
+  },
+  {
+    title: "Fight Club",
+    poster: "https://i.etsystatic.com/18242346/r/il/c9908e/2412674268/il_570xN.2412674268_1sgm.jpg",
+    rating: "8.8/10",
+    review: "A dark and stylish exploration of identity and consumerism."
+  },
+  {
+    title: "The Matrix",
+    poster: "https://c8.alamy.com/comp/PXNB40/matrix-original-movie-poster-PXNB40.jpg",
+    rating: "8.7/10",
+    review: "A groundbreaking sci-fi film that questions reality."
+  },
+  {
+    title: "Interstellar",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIPnuoxYUSbeLOfJ6D1EIn7zPj2aQN_5D2Pg&s",
+    rating: "8.6/10",
+    review: "A visually stunning journey through space and time."
+  },
+  {
+    title: "The Lord of the Rings: The Return of the King",
+    poster: "https://www.retroplace.com/pics/xbox/titles/71115--the-lord-of-the-rings-the-return-of-the-king.png",
+    rating: "9.0/10",
+    review: "An epic conclusion to the legendary fantasy trilogy."
+  }
+];
