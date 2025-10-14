833563d07732bd97e7680c9250fb5024b0ea5515
Martin <mjtitus06@gmail.com>
2025-10-14 14:05:35 +0100

    [video_player]: Added state to the presentational component to manage video loading errors - it displays error message to user on error or the selected video on success.

--- subjects/React/practice_projects/programming_patterns/video_player/src/components/Presentationals/Video.jsx
+++ subjects/React/practice_projects/programming_patterns/video_player/src/components/Presentationals/Video.jsx
@@ -1,9 +1,45 @@
-function Video({src}) {
-  return (
-    <div>
-      <video src={src} controls autostart autoPlay muted loop/>
+import { useState } from "react";
+
+function Video({ src }) {
+  //State to manage video loading errors
+  const [hasError, setHasError] = useState(false);
+
+  //Resets the error status on selecting a new video (when src changes):
+  function handleLoadStart() {
+    setHasError(false);
+  }
+
+  //Set error state to truthy on loading error:
+  function handleLoadError() {
+    setHasError(true);
+  }
+
+  //Variable holding JSX for error state.
+  const showError = (
+    <div className="video-error">
+      <p>Sorry, this video failed to load.</p>
+      <p>Please try selecting a different video.</p>
     </div>
   );
+
+  //Variable holding JSX for success state (no errors).
+  const showVideo = (
+    <div>
+      <video
+        src={src}
+        onLoadStart={handleLoadStart}
+        onError={handleLoadError}
+        controls
+        autostart
+        autoPlay
+        muted
+        loop
+      />
+    </div>
+  );
+
+  //Check for errors and display the JSX as appropriate:
+  return hasError ? showError : showVideo;
 }
 
 export default Video;
